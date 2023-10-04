c3bc2871d0a3c635e5397200c034a78e049d1901
udit-1211 <uditparekh1@gmail.com>
2023-10-04 10:00:01 +0530

    test1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const multer = require('multer');
 const s3 = require('./aws-config');
-
+require("dotenv/config");
 const app = express();
 const port = 3000;
 
